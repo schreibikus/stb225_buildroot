91a441f4c751d5c682696e736335f129b2b55fc5
Sergey Yasinsky <seregajsv@gmail.com>
2019-12-03 12:45:19 +0300

    Change remove-unneeded.sh script.

--- remove-unneeded.sh
+++ remove-unneeded.sh
@@ -6,5 +6,7 @@ rm -vf ${TARGET_DIR}/usr/bin/{ldd,getconf}
 rm -vf ${TARGET_DIR}/sbin/ldconfig
 rm -vf ${TARGET_DIR}/usr/bin/transmission-{create,edit,remote,show}
 rm -vf ${TARGET_DIR}/{lib32,usr/lib32}
-rm -vf ${TARGET_DIR}/usr/bin/{scp,sftp}
+rm -vf ${TARGET_DIR}/usr/bin/sftp
 rm -vf ${TARGET_DIR}/usr/bin/ssh{-add,-agent,-copy-id,-keyscan}
+rm -vrf ${TARGET_DIR}/etc/{rc0.d,rc2.d,rc3.d,rc5.d,xinetd.d}
+rm -vf ${TARGET_DIR}/etc/init.d/{cups,cups-browsed}
