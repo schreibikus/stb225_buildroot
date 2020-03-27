cae8bbc193c1bbe90deedb283bf0c84b2ba31235
Sergey Yasinsky <seregajsv@gmail.com>
2020-03-27 12:24:58 +0300

    Remove unneeded firmware

--- remove-unneeded.sh
+++ remove-unneeded.sh
@@ -11,3 +11,4 @@ rm -vf ${TARGET_DIR}/usr/bin/ssh{-add,-agent,-copy-id,-keyscan}
 rm -vrf ${TARGET_DIR}/etc/{rc0.d,rc2.d,rc3.d,rc5.d,xinetd.d}
 rm -vf ${TARGET_DIR}/etc/init.d/{cups,cups-browsed}
 rm -vf ${TARGET_DIR}/usr/lib/{os-release,libstdc++.so.*-gdb.py}
+rm -vf ${TARGET_DIR}/lib/firmware/rtlwifi/{rtl8192cufw_B.bin,rtl8192cufw.bin,rtl8192defw.bin,rtl8188eufw.bin,rtl8192eu_nic.bin,rtl8192cfwU.bin,rtl8192eu_wowlan.bin,rtl8188efw.bin,rtl8192eefw.bin,rtl8192cfw.bin,rtl8192cfwU_B.bin,rtl8192eu_ap_wowlan.bin,rtl8192cufw_A.bin,rtl8192sefw.bin}
